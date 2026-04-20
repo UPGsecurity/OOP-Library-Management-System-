cfe02393e76d5e591d27a2c10ff5b4d49de2b7b4
DBZ <brainxcb@gmail.com>
2026-04-20 15:06:18 +0500

    Create Author.java

---
+++ scr/main/java/library/models/Author.java
@@ -0,0 +1,23 @@
+package library.models;
+
+
+public class Author {
+    private String name;
+    private String description;
+
+    public Author(String name, String description) {
+        this.name        = name;
+        this.description = description;
+    }
+
+    public String getName()        { return name; }
+    public String getDescription() { return description; }
+
+    public void setName(String name)               { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+
+    @Override
+    public String toString() {
+        return "Author{name='" + name + "'}";
+    }
+}
