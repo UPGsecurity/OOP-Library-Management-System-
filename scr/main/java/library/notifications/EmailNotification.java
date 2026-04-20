d2acc7bec801a04eedbc2cd017937b58db5ab260
DBZ <brainxcb@gmail.com>
2026-04-20 15:23:03 +0500

    Create EmailNotification.java

---
+++ scr/main/java/library/notifications/EmailNotification.java
@@ -0,0 +1,24 @@
+package library.notifications;
+
+public class EmailNotification extends Notification {
+
+    private String email;
+
+    public EmailNotification(int notificationId, String content, String email) {
+        super(notificationId, content);
+        this.email = email;
+    }
+
+    @Override
+    public boolean sendNotification() {
+        if (email == null || !email.contains("@")) {
+            System.out.println("[XATO] Email manzil noto'g'ri: " + email);
+            return false;
+        }
+        System.out.println("[EMAIL] Yuborildi -> " + email);
+        System.out.println("        Mazmun: " + content);
+        return true;
+    }
+
+    public String getEmail() { return email; }
+}
