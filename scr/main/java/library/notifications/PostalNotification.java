f174151ac2b066160265e0190ad3b865de2190de
DBZ <brainxcb@gmail.com>
2026-04-20 15:25:15 +0500

    Create PostalNotification.java

---
+++ scr/main/java/library/notifications/PostalNotification.java
@@ -0,0 +1,26 @@
+package library.notifications;
+
+import library.models.Address;
+
+public class PostalNotification extends Notification {
+
+    private Address address;
+
+    public PostalNotification(int notificationId, String content, Address address) {
+        super(notificationId, content);
+        this.address = address;
+    }
+
+    @Override
+    public boolean sendNotification() {
+        if (address == null) {
+            System.out.println("[XATO] Manzil ko'rsatilmagan.");
+            return false;
+        }
+        System.out.println("[POCHTA] Yuborildi -> " + address.toString());
+        System.out.println("         Mazmun: " + content);
+        return true;
+    }
+
+    public Address getAddress() { return address; }
+}
